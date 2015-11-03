d552fbbf9c2b8f95ed003f9d1665906a62a69dd6
VARUN JAIN <varun13@gmail.com>
2015-11-03 23:50:56 +0530

    added logging to mongo_s recipe. added fix for instance query for config layer

--- clarifi_apps/recipes/mongo_s.rb
+++ clarifi_apps/recipes/mongo_s.rb
@@ -4,8 +4,22 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-config_instances = node["opsworks"]["layers"]["mongo-config"]["instances"].join(',')
+Chef::Log.level = :debug
 
-node["mongodb"]["sharding"]["configDB"] = config_instances
+config_instances = []
+Chef::Log.debug "Found instances found for mongo-config layer : #{node['opsworks']['layers']['mongo-config']['instances']}"
+
+node["opsworks"]["layers"]["mongo-config"]["instances"]
+node["opsworks"]["layers"]["mongo-config"]["instances"].each { |instance|
+	
+	Chef::Log.debug "instance#{}: #{instance.private_dns_name}"
+
+	config_instances.push "#{instance.private_dns_name}:27019"
+}
+
+Chef::Log.debug "config_instances: #{config_instances}"
+
+
+node["mongodb"]["sharding"]["configDB"] = config_instances.join(',')
 
 include_recipe 'mongodb::mongos'
