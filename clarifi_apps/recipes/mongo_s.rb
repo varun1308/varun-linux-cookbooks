7154e3098a689722dff1e03533011af38173e072
VARUN JAIN <varun13@gmail.com>
2015-11-04 01:00:01 +0530

    removed config instance setup as mongos recipe handles it

--- clarifi_apps/recipes/mongo_s.rb
+++ clarifi_apps/recipes/mongo_s.rb
@@ -6,4 +6,28 @@
 
 Chef::Log.level = :debug
 
+configsrvs = search(
+  :node,
+  "mongodb_cluster_name:#{node['mongodb']['cluster_name']} AND \
+   mongodb_is_configserver:true AND \
+   chef_environment:#{node.chef_environment}"
+)
+
+Chef::Log.debug "configsrvs 1:#{configsrvs}"
+
+configsrvs = search(
+  :node,
+  "mongodb_cluster_name:#{node['mongodb']['cluster_name']} AND \
+   mongodb_is_configserver:true"
+)
+
+Chef::Log.debug "configsrvs 2:#{configsrvs}"
+
+configsrvs = search(
+  :node,
+  "mongodb_cluster_name:#{node['mongodb']['cluster_name']}"
+)
+
+Chef::Log.debug "configsrvs 3:#{configsrvs}"
+
 include_recipe 'mongodb::mongos'
